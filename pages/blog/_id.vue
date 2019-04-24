c60f7b75e83841ca6905543853d7aeffbb97af69
devang <devang.patil@upgrad.com>
2019-04-24 09:20:46 +0530

    add components and pages

---
+++ pages/blog/_id.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="single-blog-post">
+        <section class="post">
+            <h1>TITLE</h1>
+            <div class="post-details">
+                <div>Written By :</div>
+            </div>
+            <p>Content</p>
+        </section>
+        <section class="post-feedback">
+            Pls Provide your comments below..
+        </section>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.admin-page {
+  padding: 20px;
+}
+.new-post {
+  text-align: center;
+  border-bottom: 2px solid #ccc;
+  padding-bottom: 10px;
+}
+.existing-posts h1 {
+  text-align: center;
+}
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/blog/blog.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>List of Blogpost</h1>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
